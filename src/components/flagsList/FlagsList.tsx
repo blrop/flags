83a047c7b34630124649278693d6f73b1bb3d11a
Igor Siluianov <olvin.hh@gmail.com>
2020-02-01 03:05:36 +0300

    make filter actually workable

--- src/components/flagsList/FlagsList.tsx
+++ src/components/flagsList/FlagsList.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 
 import { FLAGS_DIR } from "../../common/constants";
 import FlagProps from "../../common/FlagProps";
+import { getProp } from "../../common/functions";
 
 import './FlagList.scss';
 
 interface Flag {
     name: string,
     picture: string,
+    props: FlagProps,
 }
 
 interface FlagListProps {
@@ -16,9 +18,32 @@ interface FlagListProps {
 }
 
 const FlagsList: React.FC<FlagListProps> = ({ flags, filtersState }: FlagListProps) => {
-    console.log(filtersState);
+    const isFilterFieldOk = (name: keyof FlagProps, filterState: FlagProps, flagProps: FlagProps) => {
+        return !getProp(filtersState, name) || (getProp(filtersState, name) && getProp(flagProps, name));
+    };
 
-    const renderFlags = (flags: Flag[]) => flags.map(item => (
+    const renderFlags = (flags: Flag[]) => flags
+        .filter((flag: Flag) => {
+            return (
+                isFilterFieldOk('linesHorizontal', filtersState, flag.props) &&
+                isFilterFieldOk('linesVertical', filtersState, flag.props) &&
+
+                isFilterFieldOk('colorWhite', filtersState, flag.props) &&
+                isFilterFieldOk('colorBlack', filtersState, flag.props) &&
+                isFilterFieldOk('colorRed', filtersState, flag.props) &&
+                isFilterFieldOk('colorGreen', filtersState, flag.props) &&
+                isFilterFieldOk('colorBlue', filtersState, flag.props) &&
+                isFilterFieldOk('colorYellow', filtersState, flag.props) &&
+
+                isFilterFieldOk('pictureCross', filtersState, flag.props) &&
+                isFilterFieldOk('pictureStar', filtersState, flag.props) &&
+                isFilterFieldOk('pictureCircle', filtersState, flag.props) &&
+                isFilterFieldOk('pictureTriangle', filtersState, flag.props) &&
+                isFilterFieldOk('pictureCrescent', filtersState, flag.props) &&
+                isFilterFieldOk('pictureOther', filtersState, flag.props)
+            );
+        })
+        .map((item: Flag) => (
         <div className="flag-item" key={item.name}>
             <div className="flag-item__name">
                 {item.name}
